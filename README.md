fd6c0234176e89b560c61e407be469a49f6c4319
Kohei Kakimoto <naige.1028@gmail.com>
2024-12-02 23:41:36 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# StarmyKnife.Plugins.SqlServer
+
+This is a plugin for [Starmy Knife](https://github.com/sashimi343/StarmyKnife) that allows T-SQL (SQL Server) validation and prettifying.
+
+## Installation
+
+1. Download latest version of `StarmyKnife.Plugins.SqlServer.zip` from [Releases](https://github.com/sashimi343/StarmyKnife.Plugins.SqlServer/releases)
+2. Extract zip file
+3. Copy all dll in zip archive to `(StarmyKnife path)\Plugins`
+
+## Lisence
+
+This software is released under AGPL v3, see the [LICENSE.txt](LICENSE.txt) for details.
---
+++ StarmyKnife.Plugins.SqlServer/TsqlPrettyValidator.cs
@@ -0,0 +1,61 @@
+﻿using StarmyKnife.Core.Plugins;
+
+using PoorMansTSqlFormatterRedux.Tokenizers;
+using PoorMansTSqlFormatterRedux.Formatters;
+using PoorMansTSqlFormatterRedux.Parsers;
+using Microsoft.SqlServer.Management.SqlParser.Parser;
+
+namespace StarmyKnife.Plugins.SqlServer
+{
+    [StarmyKnifePlugin("TSQL (SQL Server)")]
+    public class TsqlPrettyValidator : PluginBase, IPrettyValidator
+    {
+        public bool CanPrettify => true;
+
+        public bool CanMinify => false;
+
+        public PluginInvocationResult Minify(string input, PluginParameterCollection parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PluginInvocationResult Prettify(string input, PluginParameterCollection parameters)
+        {
+            var tokenizer = new TSqlStandardTokenizer();
+            var parser = new TSqlStandardParser();
+            var formatter = new TSqlStandardFormatter();
+
+            var tokenizedSql = tokenizer.TokenizeSQL(input);
+            var parsedSql = parser.ParseSQL(tokenizedSql);
+            var formattedSql = formatter.FormatSQLTree(parsedSql);
+
+            return PluginInvocationResult.OfSuccess(formattedSql);
+        }
+
+        public ValidationResult Validate(string input, PluginParameterCollection parameters)
+        {
+            var parseResult = Parser.Parse(input);
+
+            if (parseResult.Errors.Any())
+            {
+                var errors = new List<string>();
+                foreach (var error in parseResult.Errors)
+                {
+                    var message = error.Message;
+                    var position = error.Start;
+
+                    errors.Add($"Error at position {position}: {message}");
+                }
+
+                return ValidationResult.OfFailure(errors);
+            }
+
+            return ValidationResult.OfSuccess();
+        }
+
+        protected override void ConfigureParameters(PluginParametersConfiguration configuration)
+        {
+            // No parameters
+        }
+    }
+}
